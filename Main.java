50e0b03e7ac6ca5fecc17956c77b096aa011216a
Jalen Lane <bub7684@gmail.com>
2020-09-17 14:54:06 +0000

    Just took Basic Operations and put it on the same file as the main program

--- Main.java
+++ Main.java
@@ -1,7 +1,64 @@
 import java.util.Scanner; 
 import java.util.ArrayList;
-import java.BasicOperations;
 
+public class BasicOperations  
+{
+  BasicOperations()
+  {
+    System.out.println("Welcome to basic operations!");
+    try
+    {Thread.sleep(2000);
+    } catch (exception e) 
+    {
+      System.out.println("Could not sleep!");
+    }
+  }
+  public static int Add (int a, int b) 
+  {
+    return a + b;
+  }
+
+  public static double Add (double a, double b) 
+  {
+    return a + b;
+  }
+
+  public static int Subtract (int a, int b) 
+  {
+    return a - b;
+  }
+
+  public static double Subtract (double a, double b) 
+  {
+    return a - b;
+  }
+
+  public static int Multiply (int a, int b) 
+  {
+    return a * b;
+  }
+
+  public static double Multiply (double a, double b) 
+  {
+    return a * b;
+  }
+
+  public static int Divide (int a, int b) 
+  {
+    return a / b;
+  }
+
+  public static double Divide (double a, double b) 
+  {
+    return a / b;
+  }
+
+  public static void DivideWithRemainder (int a, int b) 
+  {
+    int remainder = a % b;
+    System.out.println("The quotent is" + (a / b) + "with " + remainder + " as a remainder");
+  }
+}
 
 class Main {
   public static void main(String[] args) throws InterruptedException 
@@ -21,7 +78,7 @@ class Main {
 
     Scanner input = new Scanner(System.in);
     String decision = input.nextLine();
-  
+
     
     
   }
